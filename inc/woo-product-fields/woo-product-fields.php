99ecd9c7a1cbb9490181418fe66a6ab4db04d594
Matt Watson <matt@mwatson.co.uk>
2020-05-28 10:09:13 +0100

    WIP Woo Product Fields

--- inc/woo-product-fields/woo-product-fields.php
+++ inc/woo-product-fields/woo-product-fields.php
@@ -3,6 +3,7 @@
  * Woo Additional Product Fields.
  *
  * Example additional product fields for WooCommerce.
+ * Examples based on https://pluginrepublic.com/add-custom-cart-item-data-in-woocommerce/
  *
  * @package wholesomecode/wholesome_examples
  */
@@ -12,7 +13,14 @@
 /**
  * Setup.
  *
- * Run the hooks
+ * Run the hooks.
+ *
+ * - Remove the quantity field from the project.
+ * - Add additional product fields.
+ * - Validation for those product fields.
+ * - Add product field information to the cart.
+ * - Render product field information on the cart.
+ * - Add custom meta to the order.
  *
  * @return void
  */
@@ -26,8 +34,6 @@ function setup() : void {
 	if ( ! is_multisite() ) {
 		return;
 	}
-
-	// REF: https://pluginrepublic.com/add-custom-cart-item-data-in-woocommerce/
 
 	// Remove the quantity field.
 	add_filter( 'woocommerce_is_sold_individually', __NAMESPACE__ . '\\remove_quantity', 10, 2 );
@@ -51,11 +57,62 @@ function setup() : void {
 	// TODO.
 
 }
+
+/**
+ * Do product fields.
+ *
+ * Check that we are applying the fields to the correct product.
+ * This helps us keep the code DRY.
+ *
+ * @param int $product_id The product ID.
+ * @return boolean
+ */
+function do_product_fields( $product_id ) : bool {
+
+	/**
+	 * Example only.
+	 *
+	 * Usually we would check for a setting or meta.
+	 */
+	if ( 66 === (int) $product_id ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Remove Quantity Field.
+ *
+ * Remove the quantity field of an object.
+ *
+ * @param bool $return Remove the quantity field.
+ * @param object $product The product object.
+ * @return boolean
+ */
 function remove_quantity( $return, $product ) : bool {
+
+	if ( ! do_product_fields( $product->get_id() ) ) {
+		return false;
+	}
+
 	return true;
 }
 
+/**
+ * Add Product Fields.
+ *
+ * Add additional fields to a product on the front end.
+ *
+ * @return void
+ */
 function add_product_fields() : void {
+	global $post;
+
+	if ( ! do_product_fields( $post->ID ) ) {
+		return;
+	}
+
 	?>
 	<div class="user-email-address-wrap">
 		<label for="user-email-address">
@@ -75,7 +132,21 @@ function add_product_fields() : void {
 	<?php
 }
 
+/**
+ * Validate Product Fields.
+ *
+ * @param bool $passed If validation has passed.
+ * @param int $product_id The product ID.
+ * @param int $quantity The amount ordered.
+ * @param int $variation_id Variation ID.
+ * @return boolean
+ */
 function validate_product_fields( $passed, $product_id, $quantity, $variation_id = null ) : bool {
+
+	if ( ! do_product_fields( $product_id ) ) {
+		return $passed;
+	}
+
 	if ( empty( $_POST['user-email-address'] ) ) {
 		$passed = false;
 		wc_add_notice( esc_html__( 'Email Address is a required field.', 'wholesome-examples' ), 'error' );
@@ -87,7 +158,20 @@ function validate_product_fields( $passed, $product_id, $quantity, $variation_id
 	return $passed;
 }
 
+/**
+ * Add Cart Item Data.
+ *
+ * @param array $cart_item_data Cart Item data.
+ * @param int $product_id The product ID.
+ * @param int $variation_id The variation ID.
+ * @return array
+ */
 function add_cart_item_data( $cart_item_data, $product_id, $variation_id ) : array {
+
+	if ( ! do_product_fields( $product_id ) ) {
+		return $cart_item_data;
+	}
+
 	if ( isset( $_POST['user-email-address'] ) ) {
 		$cart_item_data['user_email_address'] = sanitize_text_field( $_POST['user-email-address'] );
 	}
@@ -97,6 +181,13 @@ function add_cart_item_data( $cart_item_data, $product_id, $variation_id ) : arr
 	return $cart_item_data;
 }
 
+/**
+ * Render Cart Item Data.
+ *
+ * @param array $item_data Item data.
+ * @param array $cart_item_data Cart item data.
+ * @return array
+ */
 function render_cart_item_data( $item_data, $cart_item_data ) : array {
 	if ( isset( $cart_item_data['user_email_address'] ) ) {
 		$item_data[] = array(
@@ -113,6 +204,15 @@ function render_cart_item_data( $item_data, $cart_item_data ) : array {
 	return $item_data;
 }
 
+/**
+ * Add Product Data to Order.
+ *
+ * @param object $item Order item.
+ * @param string $cart_item_key Cart Item Key.
+ * @param array $values Cart Values.
+ * @param int $order The Order.
+ * @return void
+ */
 function add_product_data_to_order( $item, $cart_item_key, $values, $order ) {
 	if ( isset( $values['user_email_address'] ) ) {
 		$item->add_meta_data( esc_html__( 'Email Address', 'wholesome-examples' ), $values['user_email_address'], true );
